c4d3373a5d82452641f19810703bb0567a193558
YujiKamiji <yujikamiji@gmail.com>
2025-04-24 09:36:59 -0300

    Parsers criados

--- communication/referee_receiver.py
+++ communication/referee_receiver.py
@@ -14,7 +14,7 @@ class RefereeReceiver(Receiver):
             super().__init__(ip="0.0.0.0", port=10020) #TODO, coloquei qualquer valor aqui pro ip e port
 
     def receive(self):
-        data, _ = self.sock.recvfrom(65535) #espera receber um pacote UDP de ate 65535 bytes. data contem bytes brutos recebidos
+        data, _ = self.sock.recvfrom(65535)     #espera receber um pacote UDP de ate 65535 bytes. data contem bytes brutos recebidos
         referee = referee_pb.Referee()          #instancia da classe Referee definida no .proto          
         referee.ParseFromString(data)           #metodo da biblioteca protobuf que decodifica os bytes recebidos em um obj python completo
         return referee                          #referee tem todos os atributos uteis acessiveis
