dd6a211ed74d984c79cb3f7c904cf98607c82455
YujiKamiji <yujikamiji@gmail.com>
2025-04-23 21:26:38 -0300

    Singleton no receiver

--- communication/referee_receiver.py
+++ communication/referee_receiver.py
@@ -2,8 +2,16 @@ from communication import Receiver
 from communication.generated import ssl_gc_referee_message_pb2 as referee_pb
 
 class RefereeReceiver(Receiver):
+    _instance = None 
+
+    def __new__(cls):                             #singleton
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)  # cria uma nova instância
+        return cls._instance
+
     def __init__(self):
-        super().__init__(ip="0.0.0.0", port=10003)  #TODO, coloquei qualquer valor aqui pro ip e port
+        if not hasattr(self, "sock"):
+            super().__init__(ip="0.0.0.0", port=10020) #TODO, coloquei qualquer valor aqui pro ip e port
 
     def receive(self):
         data, _ = self.sock.recvfrom(65535) #espera receber um pacote UDP de ate 65535 bytes. data contem bytes brutos recebidos
