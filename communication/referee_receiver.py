bbc69c69a356d919441477eed072c3dc4d89a89a
WesleyDosSL <wesleyleite@alunos.utfpr.edu.br>
2025-05-07 11:03:24 -0300

    Alteração da porta de entrada do Game Controller

--- communication/referee_receiver.py
+++ communication/referee_receiver.py
@@ -11,12 +11,21 @@ class RefereeReceiver(Receiver):
 
     def __init__(self):
         if not hasattr(self, "sock"):
-            super().__init__(ip="0.0.0.0", port=10020) #TODO, coloquei qualquer valor aqui pro ip e port
+            super().__init__(ip="0.0.0.0", port=10003) #TODO, coloquei qualquer valor aqui pro ip e port
+
+    #def receive(self):
+     #   data, _ = self.sock.recvfrom(65535)     #espera receber um pacote UDP de ate 65535 bytes. data contem bytes brutos recebidos
+      #  referee = referee_pb.Referee()          #instancia da classe Referee definida no .proto          
+       # referee.ParseFromString(data)           #metodo da biblioteca protobuf que decodifica os bytes recebidos em um obj python completo
+        #return referee                          #referee tem todos os atributos uteis acessiveis
 
     def receive(self):
-        data, _ = self.sock.recvfrom(65535)     #espera receber um pacote UDP de ate 65535 bytes. data contem bytes brutos recebidos
-        referee = referee_pb.Referee()          #instancia da classe Referee definida no .proto          
-        referee.ParseFromString(data)           #metodo da biblioteca protobuf que decodifica os bytes recebidos em um obj python completo
-        return referee                          #referee tem todos os atributos uteis acessiveis
+        print("Aguardando pacote do Game Controller...")
+        data, _ = self.sock.recvfrom(65535)
+        print(f"Pacote recebido com {len(data)} bytes.")
+        referee = referee_pb.Referee()
+        referee.ParseFromString(data)
+        print("Mensagem parseada com sucesso.")
+        return referee
 
     #TODO
