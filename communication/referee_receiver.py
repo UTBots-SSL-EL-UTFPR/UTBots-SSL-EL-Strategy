c26e14b75978ff358a351082a18cf6f9d0aa2a92
WesleyDosSL <wesleyleite@alunos.utfpr.edu.br>
2025-05-09 09:50:37 -0300

    Conexão Interface Ip Docker,recebendo dados brutos

--- communication/referee_receiver.py
+++ communication/referee_receiver.py
@@ -1,31 +1,26 @@
 from .receiver import Receiver
 from .generated import ssl_gc_referee_message_pb2 as referee_pb
+import socket
 
 class RefereeReceiver(Receiver):
     _instance = None 
 
-    def __new__(cls):                             #singleton
+    def __new__(cls,*args, **kwargs):                             #singleton
         if cls._instance is None:
             cls._instance = super().__new__(cls)  # cria uma nova instância
-        return cls._instance
+        return cls._instance    # Isso é útil para evitar múltiplos sockets escutando a mesma porta multicast ao mesmo tempo, o que poderia causar erros.
 
-    def __init__(self):
-        if not hasattr(self, "sock"):
-            super().__init__(ip="0.0.0.0", port=10003) #TODO, coloquei qualquer valor aqui pro ip e port
+    def __init__(self, interface_ip: str):
+        if not hasattr(self, "sock"):   #Só cria o socket se ele ainda não existe
+            super().__init__(multicast_ip="224.5.23.1", port=10003, interface_ip=interface_ip) #Configura o IP do grupo Multicast( do Game Controller) e a porta, além do IP do docker
 
-    #def receive(self):
-     #   data, _ = self.sock.recvfrom(65535)     #espera receber um pacote UDP de ate 65535 bytes. data contem bytes brutos recebidos
-      #  referee = referee_pb.Referee()          #instancia da classe Referee definida no .proto          
-       # referee.ParseFromString(data)           #metodo da biblioteca protobuf que decodifica os bytes recebidos em um obj python completo
-        #return referee                          #referee tem todos os atributos uteis acessiveis
-
-    def receive(self):
-        print("Aguardando pacote do Game Controller...")
-        data, _ = self.sock.recvfrom(65535)
-        print(f"Pacote recebido com {len(data)} bytes.")
-        referee = referee_pb.Referee()
-        referee.ParseFromString(data)
-        print("Mensagem parseada com sucesso.")
-        return referee
-
-    #TODO
+    def receive_raw(self):  # Espera e recebe pacotes brutos UDP do Game Controller
+        print("Aguardando pacote bruto do Game Controller...")
+        self.sock.settimeout(5)  # Definindo um tempo limite de 5 segundos para esperar os dados
+        try:
+            data, _ = self.sock.recvfrom(65535) # Recebe os dados brutos
+            print(f"Pacote bruto recebido com {len(data)} bytes.")
+            return data
+        except socket.timeout: #Mensagem para quando falhar ao receber os dados
+            print("Timeout: Nenhum pacote recebido.")
+            return None
--- test_referee_receiver.py
+++ test_referee_receiver.py
@@ -1,15 +1,10 @@
 from communication.referee_receiver import RefereeReceiver
 
 def test_referee_receiver():
-    receiver = RefereeReceiver()
-    message = receiver.receive()
+    receiver = RefereeReceiver(interface_ip = "172.17.0.1") # Passa o Ip de interface do docker
+    message = receiver.receive_raw() #Chama o método do Reciver que recebe os dados brutos
 
-    print("=== Mensagem Recebida ===")
-    print(f"Stage: {message.stage}")
-    print(f"Command: {message.command}")
-    print(f"Command Counter: {message.command_counter}")
-    print(f"Packet Timestamp: {message.packet_timestamp}")
-    print(f"Event: {message.event}")
+    #Comando no terminal para estabelecer a conexão do docker com o localhost: sudo docker run --network host robocupssl/ssl-game-controller -address :8081
 
 if __name__ == "__main__":
     test_referee_receiver()
