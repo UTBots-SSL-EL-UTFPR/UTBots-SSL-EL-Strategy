69144067ebdf628b82175e7a87d86fd3723bf746
WesleyDosSL <wesleyleite@alunos.utfpr.edu.br>
2025-05-16 14:25:36 -0300

    consertamos mais ou menos

--- communication/parsers/__init__.py
+++ communication/parsers/__init__.py
@@ -1,6 +1,6 @@
 # Importa e expõe a classe principal de parsing do referee
 from .referee_parser import RefereeParser
-#from .vision_parser import VisionParser
+from .vision_parser import VisionParser
 
 # Caso você tenha outros parsers no futuro, já pode preparar assim:
 # from .team_comm_parser import TeamCommParser
