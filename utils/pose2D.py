9a9da765091b36e4f61dc5c13aa190768344e0e0
WillianPontes-prog <qtgwillianp2005@gmail.com>
2025-05-29 09:51:48 -0300

    teme um bestPath bem mais funcional

--- utils/pose2D.py
+++ utils/pose2D.py
@@ -1,6 +1,8 @@
-
-
 class Pose2D:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+
+    def distance_to(self, other):
+        from math import sqrt
+        return sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
