47a64df9ed29f0d6a5bec1e769350dc90c9f6491
YujiKamiji <yujikamiji@gmail.com>
2025-06-06 15:23:27 -0300

    Refatoração dos Parsers

--- communication/parsers/referee_parser.py
+++ communication/parsers/referee_parser.py
@@ -6,6 +6,18 @@ class RefereeParser:
         # Pode ter configurações futuras, se necessário
         pass
 
+    def parse(self, raw_data: bytes) -> referee_pb.Referee: # Converte os dados brutos recebidos (bytes) em um objeto protobuf Referee.
+
+        # Nesse momento, eu optei por usar o objeto protobuf diretamente,
+        # mas poderia ser convertido para um dicionário ou outro formato, se necessário.
+        # Isso permite acessar os campos do objeto diretamente, como referee_msg.command, referee_msg.stage, etc.
+        # Mesmo assim, vou manter o método parse_to_dict para compatibilidade futura.
+    
+        referee_msg = referee_pb.Referee()
+        referee_msg.ParseFromString(raw_data)
+        return referee_msg
+
+
     def parse_to_dict(self, raw_data: bytes) -> dict:
         referee_msg = referee_pb.Referee()
         referee_msg.ParseFromString(raw_data)
