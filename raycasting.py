7ec05f34f9e894c42d6c3b6494abaca27c2de2a4
Maria Eduarda de Gracia <144639670+MEG3832@users.noreply.github.com>
2025-07-17 13:39:34 -0300

    Correções e testes finais

--- raycasting.py
+++ raycasting.py
@@ -1,15 +1,5 @@
 import math
 
-# FOI FEITO NO ÚLTIMO DIA
-    # Teste da menor solução
-    # Teste da reflexão
-    # Teste da interseção com os robôs inimigos
-    # Teste da recursão
-    
-YMAX_GOL = 0.5      #Segundo meu grSim
-YMIN_GOL = -0.5
-X_GOL = 2.250
-Y_SUP = 1.500
 RAIO_ROBO = 0.09
 N_ROBOS = 3
 
@@ -21,16 +11,23 @@ class Raio:
         self.refl = (xr, yr) 
         self.final = (xf, yf)
 
-    def __repr__(self): # Serve só para a impressão do conteúdo (teste)
-        xf, yf = self.final if self.final else (None, None)
-        xr, yr = self.refl if self.refl else (None, None)
-        return f"Raio(θ={self.angulo:.2f}, final=({xf}, {yf}), refl=({xr}, {yr}))"
-
  # Cria uma lista com todos os raios iniciais
 def criaRaios (raios, nraios, angulo_base):  
     for i in range (nraios):
         novo_raio = Raio(angulo_base*(i+1)) 
         raios.append(novo_raio)
+
+def haIntersecao (xr, yr, x0, y0, angulo):
+    a = 1
+    b = 2*(math.cos(angulo)*(x0-xr) + math.sin(angulo)*(y0-yr))
+    c = (x0 - xr)**2 + (y0-yr)**2 - RAIO_ROBO**2
+    delta = b**2 - 4*a*c # Se o delta for negativo, o raio não cruza a equação da circunferência do robo
+    if (delta >= 0):
+        t1 = (-b + math.sqrt(delta)) / (2*a)
+        t2 = (-b - math.sqrt(delta)) / (2*a)
+        if (t1 >= 0 and t2 >= 0):  # Se ambos os tempos forem positivos, o raio atinge o robô    
+            return True
+    return False
 
 def ehRoboValido (coord_aliados, x0, y0, xa, ya, j):
     # Verifica se o robô em análise não é o robô que chuta a bola
@@ -45,51 +42,37 @@ def ehRoboValido (coord_aliados, x0, y0, xa, ya, j):
 def atingiuInimigo (raios, x0, y0, coord_inimigos):
     for raio in raios[:]:   # Cria uma cópia da lista para evitar problemas de remoção durante a iteração
         angulo = raio.angulo
-        for j in range (N_ROBOS):   # Vê se colide com cada um dos robôs
-            if raio in raios:  # Verifica se o raio ainda está na lista (pode ter sido removido em aluma das iterações anteriores)
-                xr = coord_inimigos[j][0]
-                yr = coord_inimigos[j][1]
-                a = 1
-                b = 2*(math.cos(angulo)*(x0-xr) + math.sin(angulo)*(y0-yr))
-                c = (x0 - xr)**2 + (y0-yr)**2 - RAIO_ROBO**2
-                delta = b**2 - 4*a*c # Se o delta for negativo, o raio não cruza a equação da circunferência do robo
-                if (delta >= 0):
-                    t1 = (-b + math.sqrt(delta)) / (2*a)
-                    t2 = (-b - math.sqrt(delta)) / (2*a)
-                    if (t1 >= 0 and t2 >= 0):  # Se ambos os tempos forem positivos, o raio atinge o robô
-                        raios.remove(raio)    # Adiciona a lista o raio que reflete e as coordenadas do robô no qual bate
+        for i in range (N_ROBOS):   # Vê se colide com cada um dos robôs
+            if raio in raios:  # Verifica se o raio ainda está na lista (pode ter sido removido em alguma das iterações anteriores)
+                xr = coord_inimigos[i][0]
+                yr = coord_inimigos[i][1]
+                if haIntersecao(xr, yr, x0, y0, angulo):  # Se o raio não atinge o robô, não precisa mais analisar esse raio
+                    raios.remove(raio)    # Adiciona a lista o raio que reflete e as coordenadas do robô no qual bate
 
 # Determina se a bola entrou no gol. Acessa o último ângulo de cada uma das trajetórias e analisa cada um deles. Se o raio for vertical, pula, pois não
 # cruza nenhuma linha do gol. Descobre o parâmetro quando a bola cruza a linha do gol a partir da expressão x(t) = x_bola + t*cos()
 #  e usa ele para encontrar onde o raio cruza o a linha de trás do gol. Salva a trajetória cujo último ângulo atravessa o gol e o ponto onde isso ocorre.
-def entrouGol (solucoes, raios, x0, y0):
+def entrouGol (x_gol, ymin_gol, ymax_gol, solucoes, raios, x0, y0):
     for raio in raios:
         angulo = raio.angulo    
         if (angulo != math.pi/2 and angulo != math.pi*3/2):
-            t = (X_GOL - x0) / math.cos(angulo)
+            t = (x_gol - x0) / math.cos(angulo)
             y = y0 + t * math.sin(angulo) 
-            if ((y < YMAX_GOL and y > YMIN_GOL) and t >= 0):
+            if ((y < ymax_gol and y > ymin_gol) and t >= 0):
                 solucoes.append([raio])
-                raio.final = (X_GOL, y)
+                raio.final = (x_gol, y)
 
 # Joga a expressão paramétrica do raio na expressão do círulo do robô. Simplificando fica t² + 2*(math.cos(angulo)*(x_bola-x_robo) + math.sin(angulo)(y_bola-y_robo))*t + x_bola*(x_bola - 2*x_raio) + y_bola*(y_bola - 2*y_raio) - RAIO_ROBO
 def atingiuAliado (refletiu, raios, x0, y0, coord_aliados, xa = None, ya = None):
     for raio in raios:
         angulo = raio.angulo    # Acessa o último ângulo de cada uma das trajetórias (ou seja, só analisa o último raio)
-        for j in range (N_ROBOS):   # Vê se colide com cada um dos robôs
-            if ehRoboValido(coord_aliados, x0, y0, xa, ya, j):
-                xr = coord_aliados[j][0]
-                yr = coord_aliados[j][1]
-                a = 1
-                b = 2*(math.cos(angulo)*(x0 - xr) + math.sin(angulo)*(y0 - yr))
-                c = (x0 - xr)**2 + (y0 - yr)**2 - RAIO_ROBO**2
-                delta = b**2 - 4*a*c # Se o delta for negativo, o raio não cruza a equação da circunferência do robo
-                if (delta >= 0):
-                    t1 = (-b + math.sqrt(delta)) / (2*a)
-                    t2 = (-b - math.sqrt(delta)) / (2*a)
-                    if (t1 >= 0 and t2 >= 0):  # Se ambos os tempos forem positivos, o raio atinge o robô
-                        refletiu.append(raio)    # Adiciona a lista o raio que reflete e as coordenadas do robô no qual bate
-                        raio.refl = (xr, yr)
+        for i in range (N_ROBOS):   # Vê se colide com cada um dos robôs
+            if ehRoboValido(coord_aliados, x0, y0, xa, ya, i):
+                xr = coord_aliados[i][0]
+                yr = coord_aliados[i][1]
+                if haIntersecao(xr, yr, x0, y0, angulo):
+                    refletiu.append(raio)    # Adiciona a lista o raio que reflete e as coordenadas do robô no qual bate
+                    raio.refl = (xr, yr)
 
 # Vê qual das soluções tem o menor módulo
 def menorSolucao(solucoes, x0, y0, nsol):
@@ -117,7 +100,7 @@ def menorSolucao(solucoes, x0, y0, nsol):
     return menor_trajetoria
 
 # Cria varios raios que saem da bola. Depois de ver se algum acerta o gol, calcula o de menor módulo entre eles. Ainda falta adicionar osbtáculos (última coisa) e refletir quando bate em um aliado)        
-def menorTrajetoria (x0, y0, coord_aliados, coord_inimigos, lim_refl, xa = None, ya = None): 
+def menorTrajetoria (ymax_gol, ymin_gol, x_gol, x0, y0, coord_aliados, coord_inimigos, lim_refl, xa = None, ya = None): 
     nraios = 8 
     solucoes = []
     refletiu = []
@@ -129,7 +112,7 @@ def menorTrajetoria (x0, y0, coord_aliados, coord_inimigos, lim_refl, xa = None,
 
     # Descarta raios que atingem os robôs inimigos e verifica se a bola entrou no gol ou se reflete
     atingiuInimigo (raios, x0, y0, coord_inimigos)
-    entrouGol(solucoes, raios, x0, y0)    
+    entrouGol(x_gol, ymin_gol, ymax_gol, solucoes, raios, x0, y0)    
     atingiuAliado (refletiu, raios, x0, y0, coord_aliados, xa, ya)
 
     # Se não houver soluções, verifica se algum raio refletiu e, através da recursão, analisa se há alguma
@@ -139,7 +122,7 @@ def menorTrajetoria (x0, y0, coord_aliados, coord_inimigos, lim_refl, xa = None,
         for i in range(nrefl):
             menor_caminho = None
             if (lim_refl < (N_ROBOS - 1)):  # Limita o nº de reflexões para que a bola não volte para algum robô que já jogou
-                menor_caminho = menorTrajetoria(refletiu[i].refl[0], refletiu[i].refl[1], coord_aliados, coord_inimigos, lim_refl + 1, x0, y0)
+                menor_caminho = menorTrajetoria(ymax_gol, ymin_gol, x_gol, refletiu[i].refl[0], refletiu[i].refl[1], coord_aliados, coord_inimigos, lim_refl + 1, x0, y0)
             if (menor_caminho != None):
                 trajetoria = []
                 # A trajetória passa a ser o raio que refletiu mais a trajetória de menor módulo retornada da recursão
@@ -152,16 +135,3 @@ def menorTrajetoria (x0, y0, coord_aliados, coord_inimigos, lim_refl, xa = None,
         # Se houver soluções, retorna a de menor módulo
         return menorSolucao(solucoes, x0, y0, nsol)
     return None
-
-# TESTE
-# Inicializando parâmetros que devem ser fornecidos pelo SSL vision, mas estou colocando aqui só para teste
-# Coordenadas do robô que chuta a bola
-#if __name__ == "__main__":
-#    x0 = 0
-#    y0 = 0
-#    coord_aliados = [(0, 0), (1, 1), (1.5, 1)] # Matriz [N_ROBOS X 2]
-#    coord_inimigos = [(0.5, 0), (1, -0.25), (1.5, 0.25)] # Matriz [N_INIMIGOS X 2]
-#    for i, (x, y) in enumerate(coord_aliados): # Teste
-#        print(f"Robô {i}: x = {x}, y = {y}")
-#
-#    print("Trajetória de menor módulo:", menorTrajetoria(x0, y0, coord_aliados, coord_inimigos, 0))
