fb5f22fbb77a0b80e7c2cb3692e2329d0de4cb5f
argenton <joaopedroargenton@hotmail.com>
2025-08-19 10:22:26 -0300

    att pose 2d

--- utils/Pose2D.py
+++ utils/Pose2D.py
@@ -1,11 +1,28 @@
+from utils.defines import (
+    ALL_QUADRANTS,
+    Quadrant_type,
+    RoleType,
+    ATTACK_QUADRANTS,
+    MIDFIELD_QUADRANTS,
+    DEFENSE_QUADRANTS,
+    GOALKEEPER_ZONE,
+    BALL_POSSESSION_DISTANCE,
+)
 
 class Pose2D:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
+    def __init__(self, x=0, y=0, teta=0):
+        self.x = x if x is not None else 0
+        self.y = y if y is not None else 0
+        self.teta = teta if teta is not None else 0
+
     #---------------------------------------------------------------------------------------#
     #                                       SOBRECARGAS                                     #
     #---------------------------------------------------------------------------------------#
+    
+    def __iter__(self): #DESEMPACOTAMENTO 
+        yield self.x
+        yield self.y
+        yield self.teta
 
     def __repr__(self):
         return f"Point2D(x={self.x}, y={self.y}), "
@@ -20,7 +37,6 @@ class Pose2D:
             return NotImplemented
         return Pose2D(self.x - other.x, self.y - other.y)
 
-    
     def __iadd__(self, other):# Ponto += Ponto
         if not isinstance(other, Pose2D):
             return NotImplemented
@@ -39,7 +55,18 @@ class Pose2D:
         if not isinstance(other, Pose2D):
             return False
         return self.x == other.x and self.y == other.y
+    
+    #---------------------------------------------------------------------------------------#
+    #                                   METODOS AUXILIARES                                  #
+    #---------------------------------------------------------------------------------------#
 
     def distance_to(self, other):
         from math import sqrt
         return sqrt((self.x - other.x)**2 + (self.y - other.y)**2)
+    
+    def get_quadrant(self) -> int:
+        """Calcula e retorna o índice (1..12) do quadrante atual ou 0 se fora do campo."""
+        for i, (x_min, x_max, y_min, y_max) in enumerate(ALL_QUADRANTS, 1):
+            if x_min <= self.x <= x_max and y_min <= self.y <= y_max:
+                return i
+        return 0
