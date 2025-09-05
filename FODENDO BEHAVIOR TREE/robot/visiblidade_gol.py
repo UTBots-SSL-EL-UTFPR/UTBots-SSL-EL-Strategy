e26f277762538f8b42f34b871d72bd57b46773b5
WesleyDosSL <wesleyleite@alunos.utfpr.edu.br>
2025-09-05 16:10:09 -0300

    Finalização do código (ainda falta testar e verificar se é funcional para x_gol < 0)

--- FODENDO BEHAVIOR TREE/robot/visiblidade_gol.py
+++ FODENDO BEHAVIOR TREE/robot/visiblidade_gol.py
@@ -2,6 +2,15 @@ import math
 
 ROBOT_RADIUS = 85   # está em mm
 
+class Obstacle:
+    def __init__(self, xr, yr , theta_bottom = -1, theta_top = -1):
+        self.xr = xr
+        self.yr = yr
+        # Ângulos das retas tangentes ao bob e que passam por (x0, y0)
+        self.theta_bottom = theta_bottom    
+        self.theta_top = theta_top
+
+# Estou apenas reutilizando uma função que eu fiz em ray_casting
 def haIntersecao (xr, yr, x0, y0, angulo):
     a = 1
     b = 2*(math.cos(angulo)*(x0-xr) + math.sin(angulo)*(y0-yr))
@@ -14,23 +23,25 @@ def haIntersecao (xr, yr, x0, y0, angulo):
             return True
     return False
 
-# Calcua
-def calc_visible_bobs(x0, y0, obstacles, x_gol, y_golMin, y_golMax, theta_max, theta_min):
+# Calcua ums lista com os obstáculos que estão no campo de visão do chutador
+def calc_visible_bobs(x0, y0, obstacles_coord, x_gol, y_golMin, y_golMax, theta_max, theta_min):
     visible_bobs = []
     for i in range(len(obstacles)):
-        xr = obstacles[i][0]
-        yr = obstacles[i][1]
+        xr = obstacles_coord[i][0]
+        yr = obstacles_coord[i][1]
         if((x_gol > 0 and xr < 0) or (x_gol < 0 and xr > 0)):
             continue
         m = (yr-y0) / (xr-x0)
         intersec_y = m(x_gol - x0) + y0
-        if (intersec_y >= y_golMin or intersec_y <= y_golMax):
+        if (intersec_y >= y_golMin and intersec_y <= y_golMax):
             visible_bobs.insert((xr, yr))
         elif (haIntersecao(xr, yr, x0, y0, theta_min) or haIntersecao(xr, yr, x0, y0, theta_max)):
-              visible_bobs.insert((xr, yr))
+              theta_bottom, theta_top = ang_tangent_lines(x0, y0, xr, yr)
+              bob = Obstacle(xr, yr, theta_bottom, theta_top)
+              visible_bobs.insert(bob)
     return visible_bobs
 
-# calcula o ângulo das retas tangentes à circunferência
+# Calcula o ângulo das retas tangentes à circunferência
 def ang_tangent_lines(x0, y0, xr, yr, theta):
     a = ROBOT_RADIUS**2 - (xr-x0)**2
     b = -2*(xr-y0)*(y0-yr)
@@ -39,26 +50,41 @@ def ang_tangent_lines(x0, y0, xr, yr, theta):
     if (delta >= 0):
         m1 = (-b + math.sqrt(delta)) / (2*a)
         m2 = (-b - math.sqrt(delta)) / (2*a)
-        theta1 = math.atan(m1) - theta
-        theta2 = math.atan(m2) - theta
-        return (min(theta1, theta2), max(theta, theta2))
-    # Preciso retornar algo em caso de erro ou o Phyton faz isso por mim?
+        theta1 = math.atan(m1)
+        theta2 = math.atan(m2)
+        return theta1, theta2
 
-# retorna o maior intervalo de visão
+# Retorna o maior intervalo de visão
 def is_visible(obstacles, p0, x_gol, y_golMin, y_golMax):
     x0 = p0[0]    # Posição dop robô  que está chutando
     y0 = p0[1]
-    d = math.sqrt(y0**2 + (x_gol-x0)**2) # Distância em relação ao meio do gol
 
-    # Ângulo entre os lims. do gol e o bob chutando
+    # Ângulo entre os lims. do gol e o chutador
     theta_max = math.atan((y_golMax - y0)/((x_gol - x0)))
     theta_min = math.atan((y_golMin - y0)/((x_gol - x0))) 
-    delta_theta = (theta_max - theta_min)/d    # O n° de raios lançados é igual à distância (em mm) do gol
     theta = theta_min
-    angulo_chute_aux = [-1, -1]
-    angulo_chute = [-1,-1]  # Lista com o início e o fim do intervalo de visão
+    kick_angle = [-1,-1]  # Lista com o início e o fim do intervalo de visão
     
     visible_bobs = calc_visible_bobs(x0, y0, obstacles, x_gol, y_golMin, y_golMax, theta_max, theta_min)
+    visible_bobs.sort(key=lambda bob: bob.theta_bottom) # Organiza a lista em ordem crescente do ângulo da reta tangência à parte inferior do bob
+
+    if(visible_bobs == None):
+        kick_angle = [theta, theta_max]
+    else:
+        # Varre o ângulo entre a parte de cima de um bob e a parte de baixo de outro (armazena o maior intervalo)
+        for i in range(len(visible_bobs)):
+            theta_bottom = visible_bobs[i].theta_bottom
+            theta_top = visible_bobs[i].theta_top
+            if((theta_bottom - theta) >=0 and (theta_bottom - theta) > (kick_angle[1] - kick_angle[0])):
+                kick_angle = [theta, theta_bottom]
+            theta = theta_top
+            if(theta >= theta_max):
+                break
+            elif((i == len(visible_bobs)-1) and theta < theta_max):
+                if(theta_max - theta > kick_angle[1] - kick_angle[0]):
+                    kick_angle = [theta, theta_bottom]
+                    
+    return kick_angle
 
 # Teste
 if __name__ == "__main__":
