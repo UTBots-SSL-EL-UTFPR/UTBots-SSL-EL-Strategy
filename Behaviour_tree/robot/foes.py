7aa8d3f22d4c22aff508ccbbfdc9e7bdb7d603fc
Vinicius <viniciusferreiramatias79@gmail.com>
2025-09-25 15:08:51 -0300

    organizando de forma parecida com o bob_state

--- Behaviour_tree/robot/foes.py
+++ Behaviour_tree/robot/foes.py
@@ -37,97 +37,21 @@ class Foes_State:
         self.has_ball = False
         self.position_rept = 0
 
+    def setup_callbacks(self):
+
+        event_callbacks.lost_ball_posetion(self.robot_id.name)
+        event_callbacks.new_quadrant(self.robot_id.name, 0)
+        event_callbacks.new_zone(self.robot_id.name, 0)
+        event_callbacks.on_robot_stuck(self.robot_id.name)
+        event_callbacks.target_reset(self.robot_id.name)
+        event_callbacks.on_ball_not_visible(self.robot_id.name, Pose2D(0, 0))
+
     def update(self):
-        self.position = self.world_state.get_foe_robot_pose(self.robot_id.value)
-        self.velocity = self.world_state.get_foe_robot_velocity(self.robot_id.value)
-
-        ################# Verifica se a posse de bola foi alterada #################
-        if self.has_ball != self.check_ball_possession():
-            if(self.has_ball):
-                event_callbacks.lost_ball_posetion(self.robot_id.name)
-            else:
-                self.ball_visible = True
-                event_callbacks.team_got_ball_posetion(self.robot_id.name)
-            self.has_ball = not self.has_ball
-
-
-        ################# Verifica se a existe uma linha de passe #################  
-
-        #primeiro a bola  esta no goleiro 
-        if self.robot_id == RobotID(2):
-            pos_gol=self.get_position()
-            pos_1=World_State.get_team_robot_pose(self,1)
-            pos_2=World_State.get_team_robot_pose(self,0)
-            
-            obstacles = self.world_state.get_all_foes_position()
-            
-            if(Positioning_helper.is_path_clear(pos_gol,pos_1,obstacles,ROBOT_RADIUS) or Positioning_helper.is_path_clear(pos_gol,pos_2,obstacles,ROBOT_RADIUS)):
-                event_callbacks.on_valid_line(self.robot_id.name)
-        #se a bola esta com outro robo
-        elif self.robot_id == RobotID(1):
-            pos_1=self.get_position()
-            pos_2=World_State.get_team_robot_pose(self,0)
-        
-            
-            obstacles =self.world_state.get_all_foes_position()
-             
-            if(Positioning_helper.is_path_clear(pos_1,pos_2,obstacles,ROBOT_RADIUS)):
-                event_callbacks.on_valid_line(self.robot_id.name)
-
-        else:
-            pos_1=self.get_position()
-            pos_2=World_State.get_team_robot_pose(self,1)
-        
-            
-            obstacles = self.world_state.get_all_foes_position()
-             
-            if(Positioning_helper.is_path_clear(pos_1,pos_2,obstacles,ROBOT_RADIUS)):
-                event_callbacks.on_valid_line(self.robot_id.name)
-                    
-
-        #################   Verifica se preso na mesma pos e verifica quadrante   #################
-        new_pos = self.world_state.get_team_robot_pose(self.robot_id.value)
-        if new_pos is None:
-            return
-
-        if self.position == new_pos:
-            self.position_rept += 1
-        else:
-            self.position_rept = 0
-            #---------------------quadrante---------------------#
-            if new_pos.quadrant != self.position.quadrant:
-                event_callbacks.new_quadrant(self.robot_id.name, new_pos.quadrant)
-                if new_pos.quadrant != self.position.zone:
-                    event_callbacks.new_zone(self.robot_id.name, new_pos.zone)
-            self.position = new_pos
-
-        if self.position_rept >= 15:
-            self.position_rept = 0
-            event_callbacks.on_robot_stuck(self.robot_id.name)
-
-        #################   Verifica visão da bola   #################
-        visible, best_position = Positioning_helper.get_clear_pass_position(self.position)
-        if visible != self.ball_visible:
-            if visible: 
-                event_callbacks.on_ball_visible(self.robot_id.name)
-            else:
-                event_callbacks.on_ball_not_visible(self.robot_id.name, best_position) # provavelmente teria que usar uma versão diferente
-
-            self.ball_visible = visible
-
-        
-
-        new_vel = self.world_state.get_team_robot_velocity(self.robot_id.value)
-        if new_vel is not None:
-            self.velocity = new_vel
-
-        #TODO CHAMAR FUNÇÃO PARA VERIFICAR VISIBILIDADE BOLA - GOL
-        #TODO CHAMAR FUNÇÃO PARA VISIBILIDADE DE PASSE 
-        #TODO IMPLEMENTAR MOVIMENTAÇÃO
-        #   -> TEMSO QUE ACOMPANHAR O MOVIMENTO DO BOB, MAS O TICK DA ARVORE VAI SER LENTO
-        #   -> PODEMOS OU CHAMAR UMA THREAD OU FAZER UM ESQUEMA DE MOVIMENTAÇÃO DENTRO DA UPDATE
-        #   -> PODEMOS FAZER ELE SEMPRE SEGUIR O OBJETIVO DELE, EM UMA LISTA DE POS
-        #   -> A UPDATE VAI SER CHAMADA TODOS OS CICLOS, ENTÃO PODEMOS USA-LA PARA MOV.
+        self.is_ball_with_robot()
+        self.is_robot_stuck()
+        self.is_visible_from_ball()
+        self.update_velocity()
+        self.is_ball_reachable()
 
      # ---------------------------------------------------------------------------------------#
     #                                         Setters                                       #
@@ -163,6 +87,12 @@ class Foes_State:
         self.role = None
         self.setup_callbacks()
 
+    # =================== Updates ===================
+    def update_velocity(self):       
+        new_vel = self.world_state.get_team_robot_velocity(self.robot_id.value)
+        if new_vel is not None:
+            self.velocity = new_vel
+
     # =================== Métricas / consultas ===================
     def check_ball_possession(self) -> bool:
         ball_position = self.world_state.get_ball_position()
@@ -170,7 +100,108 @@ class Foes_State:
             return self.position.distance_to(ball_position) <= BALL_POSSESSION_DISTANCE
         print("ERRO, POS da BOLA OU do ROBO NULOS")
         return False
+    
+    def is_robot_stuck(self):
+                #################   Verifica se preso na mesma pos e verifica quadrante   #################
+        new_pos = self.world_state.get_team_robot_pose(self.robot_id.value)
+        if new_pos is None:
+            return
 
+        if self.position == new_pos:
+            self.position_rept += 1
+        else:
+            self.position_rept = 0
+            #---------------------quadrante---------------------#
+            if new_pos.quadrant != self.position.quadrant:
+                event_callbacks.new_quadrant(self.robot_id.name, new_pos.quadrant)
+                if new_pos.quadrant != self.position.zone:
+                    event_callbacks.new_zone(self.robot_id.name, new_pos.zone)
+            self.position = new_pos
+
+        if self.position_rept >= 15:
+            self.position_rept = 0
+            event_callbacks.on_robot_stuck(self.robot_id.name)
+
+    def is_visible_from_ball(self):
+        visible, best_position = Positioning_helper.get_clear_pass_position(
+            self.position
+        )
+        if visible != self.ball_visible:
+            if visible:
+                event_callbacks.on_ball_visible(self.robot_id.name)
+            else:
+                event_callbacks.on_ball_not_visible(self.robot_id.name, best_position)
+
+        self.ball_visible = visible
+
+    def is_ball_reachable(self):
+        reachable = 500 > self.position.distance_to(
+            self.world_state.get_ball_position()
+        )
+        event_callbacks.on_ball_reachable(self.robot_id.name, reachable)
+
+    def is_ball_with_robot(self):
+        if self.has_ball != self.check_ball_possession():
+            if self.has_ball:
+                event_callbacks.lost_ball_posetion(self.robot_id.name)
+            else:
+                self.ball_visible = True
+                event_callbacks.team_got_ball_posetion(self.robot_id.name)
+            self.has_ball = not self.has_ball
+
+
+        ################# Verifica se a existe uma linha de passe #################  
+
+        #primeiro a bola  esta no goleiro 
+        if self.robot_id == RobotID(2):
+            pos_gol=self.get_position()
+            pos_1=World_State.get_team_robot_pose(self,1)
+            pos_2=World_State.get_team_robot_pose(self,0)
+            
+            obstacles = self.world_state.get_all_foes_position()
+            
+            if(Positioning_helper.is_path_clear(pos_gol,pos_1,obstacles,bob.ROBOT_RADIUS) or Positioning_helper.is_path_clear(pos_gol,pos_2,obstacles,bob.ROBOT_RADIUS)):
+                event_callbacks.on_valid_line(self.robot_id.name)
+        #se a bola esta com outro robo
+        elif self.robot_id == RobotID(1):
+            pos_1=self.get_position()
+            pos_2=World_State.get_team_robot_pose(self,0)
+        
+            
+            obstacles =self.world_state.get_all_foes_position()
+             
+            if(Positioning_helper.is_path_clear(pos_1,pos_2,obstacles,bob.ROBOT_RADIUS)):
+                event_callbacks.on_valid_line(self.robot_id.name)
+
+        else:
+            pos_1=self.get_position()
+            pos_2=World_State.get_team_robot_pose(self,1)
+        
+            
+            obstacles = self.world_state.get_all_foes_position()
+             
+            if(Positioning_helper.is_path_clear(pos_1,pos_2,obstacles,bob.ROBOT_RADIUS)):
+                event_callbacks.on_valid_line(self.robot_id.name)
+
+
+        #################   Verifica se preso na mesma pos e verifica quadrante   #################
+        new_pos = self.world_state.get_team_robot_pose(self.robot_id.value)
+        if new_pos is None:
+            return
+
+        if self.position == new_pos:
+            self.position_rept += 1
+        else:
+            self.position_rept = 0
+            if new_pos.quadrant != self.position.quadrant:
+                event_callbacks.new_quadrant(self.robot_id.name, new_pos.quadrant)
+                if new_pos.quadrant != self.position.zone:
+                    event_callbacks.new_zone(self.robot_id.name, new_pos.zone)
+            self.position = new_pos
+
+        if self.position_rept >= 15:
+            self.position_rept = 0
+            event_callbacks.on_robot_stuck(self.robot_id.name)
     # =================== Getters simples para agregador ===================
     def get_position(self)-> Pose2D:
         return self.position
