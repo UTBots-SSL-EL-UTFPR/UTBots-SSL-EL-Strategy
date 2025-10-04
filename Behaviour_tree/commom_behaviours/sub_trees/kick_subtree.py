4d1b22531e17a74ad8d7436d29fd0f710d21dd86
MEG3832 <dudagracia11@gmail.com>
2025-10-04 15:49:41 -0300

    Mudei o teste para o modelo do test_off_sup.py

--- Behaviour_tree/commom_behaviours/sub_trees/kick_subtree.py
+++ Behaviour_tree/commom_behaviours/sub_trees/kick_subtree.py
@@ -1,62 +1,30 @@
 # kick subtree.py
 import py_trees
 from Behaviour_tree.core.World_State import RobotID
-from Behaviour_tree.core.blackboard import Blackboard_Manager
 from Behaviour_tree.bob_manager import BobManager
+from Behaviour_tree.robot.bob import Bob
+
 
 from Behaviour_tree.commom_behaviours import condition as condition_nodes
 from Behaviour_tree.commom_behaviours import actions as action_nodes
+from Behaviour_tree import commom_behaviours as cb
 
+def get_kick_subtree(robot: Bob) -> py_trees.trees.BehaviourTree:
+    posse_aliada = cb.condition.TeamHasBall()
+    visibilidade_gol = cb.condition.Goal_visibility(attacker=robot, name="Ve o gol")
+    distancia_gol = cb.condition.Goal_distance(attacker=robot, name="Ve o gol")
 
-class KickTree :
-    def __init__(self, robot_id: RobotID):
-        self.robot_id = robot_id
-        self.bob_manager = BobManager.get_object()
-        self.robot = self.bob_manager.get_bob(self.robot_id)
-        
-        if self.robot is None:
-            raise ValueError(f"Robô com ID {robot_id} não encontrado.")
+    alinhar_com_gol = cb.actions.Align_for_shoot(robot)
+    chutar_gol = cb.actions.Shoot_to_goal(robot)
+    kick_subtree = py_trees.composites.Sequence(
+        "chutar no gol",
+        True,
+        children=[posse_aliada, visibilidade_gol, distancia_gol, alinhar_com_gol, chutar_gol],
+    )
 
+    kick_root = py_trees.trees.BehaviourTree(kick_subtree)
+    kick_root.setup()
 
-    def create_tree(self) -> py_trees.behaviour.Behaviour:
-        if not self.robot:
-            return py_trees.behaviours.Failure("Robô não inicializado")
+    return kick_root
 
-    # CONDIÇÕES
-        has_ball = condition_nodes.HasBall(robot=self.robot, name="Tem a bola")
-        goal_visibility = condition_nodes.Goal_visibility(attacker=self.robot, name="Ve o gol")
-        goal_distance = condition_nodes.Goal_distance(attacker=self.robot, name="Distancia Valida")
-        kick_conditions = py_trees.composites.Sequence(
-            "Condicoes de chute",
-             memory=True,
-             children=[
-                 has_ball,
-                 goal_visibility,
-                 goal_distance
-                 ]
-        )
-
-    # AÇÕES
-        align_for_shoot = action_nodes.Align_for_shoot(attacker=self.robot, name="Alinha com o gol")
-        shoot_to_goal = action_nodes.Shoot_to_goal(attacker=self.robot, name="Chuta no gol")
-        kick_actions = py_trees.composites.Sequence(
-            "Acoes de chute",
-            memory=True,
-            children=[
-                align_for_shoot,
-                shoot_to_goal
-                ]
-        )
-
-    # RAÍZ
-        kick_subtree = py_trees.composites.Sequence(
-            "arvore de chute",
-            memory=True,
-            children=[
-                kick_conditions,
-                kick_actions
-                ]
-        )
-        return kick_subtree
-    
 
