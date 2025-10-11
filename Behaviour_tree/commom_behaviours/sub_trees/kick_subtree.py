f6e8c2291151091f9fd304650e77f5d0364d2d20
Maria Eduarda de Gracia <144639670+MEG3832@users.noreply.github.com>
2025-10-11 13:46:45 -0300

    Mudei os nós de ação

--- Behaviour_tree/commom_behaviours/sub_trees/kick_subtree.py
+++ Behaviour_tree/commom_behaviours/sub_trees/kick_subtree.py
@@ -1,29 +1,33 @@
 # kick subtree.py
 import py_trees
-
-from Behaviour_tree import commom_behaviours as cb
-from Behaviour_tree.bob_manager import BobManager
-from Behaviour_tree.commom_behaviours import actions as action_nodes
-from Behaviour_tree.commom_behaviours import condition as condition_nodes
 from Behaviour_tree.core.World_State import RobotID
+from Behaviour_tree.bob_manager import BobManager
 from Behaviour_tree.robot.bob import Bob
 
 
+from Behaviour_tree.commom_behaviours import condition as condition_nodes
+from Behaviour_tree.commom_behaviours import actions as action_nodes
+from Behaviour_tree import commom_behaviours as cb
+
 def get_kick_subtree(robot: Bob) -> py_trees.trees.BehaviourTree:
-    # posse_aliada = cb.condition.HasBall(robot)
+    posse_da_bola = cb.condition.HasBall(robot)
     visibilidade_gol = cb.condition.Goal_visibility(attacker=robot, name="Ve o gol")
     distancia_gol = cb.condition.Goal_distance(attacker=robot, name="Ve o gol")
 
-    calcular_target_position = cb.actions.Calculate_kick_target(robot)
-    alinhar_com_gol = cb.actions.Align(robot)
+    calculate_angular_position = cb.actions.Calculate_angular_target(robot)
+    calculate_linear_position = cb.actions.Calculate_linear_target(robot)
+    alinhamento_angular = cb.actions.Angular_align(robot)
+    alinhamento_linear = cb.actions.Move_node(robot)
     chutar_gol = cb.actions.Shoot_to_goal(robot)
     kick_subtree = py_trees.composites.Sequence(
         "chutar no gol",
         True,
-        children=[visibilidade_gol, distancia_gol, alinhar_com_gol, chutar_gol],
+        children=[posse_da_bola, visibilidade_gol, distancia_gol, calculate_linear_position, alinhamento_linear, calculate_angular_position, alinhamento_angular, chutar_gol]
     )
 
     kick_root = py_trees.trees.BehaviourTree(kick_subtree)
     kick_root.setup()
 
     return kick_root
+
+
