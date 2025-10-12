5703ec3a7de27ae5b2c9455f3b986c2fe798c5a4
Maria Eduarda de Gracia <144639670+MEG3832@users.noreply.github.com>
2025-10-12 17:06:05 -0300

    Mudanças com o teste

--- Behaviour_tree/helpers/visiblidade_gol.py
+++ Behaviour_tree/helpers/visiblidade_gol.py
@@ -5,23 +5,20 @@
 """
 import math
 from utils.pose2D import Pose2D
+from utils.defines import (ROBOT_RADIUS)
+from .field_helper import (GOAL_LENGHT)
 
-
-# está em mm
-ROBOT_RADIUS = 85 
-YGOL_MIN = -750
-YGOL_MAX = 750
-
+VISIBILIDADE_MIN = 3  # em graus
 
 """_summary_: Classe dos obstáculos, que guarda suas posições (x, y) e os
 ângulos das suas retas tangentes que passam por (x0, y0)
 """
 class Obstacle:
-   def __init__(self, xr, yr, theta_bottom=-1, theta_top=-1):
-       self.xr = xr
-       self.yr = yr
-       self.theta_bottom = theta_bottom
-       self.theta_top = theta_top
+    def __init__(self, xr, yr, theta_bottom=-1, theta_top=-1):
+        self.xr = xr
+        self.yr = yr
+        self.theta_bottom = theta_bottom
+        self.theta_top = theta_top
 
 
 """_summary_: Combina as expressões (x=x0+cos(angulo), y=y0+sen(angulo)) com
@@ -31,15 +28,15 @@ class Obstacle:
    return _type_: bool
 """
 def haIntersecao(xr, yr, x0, y0, angulo):
-   a = 1
-   b = 2 * (math.cos(angulo) * (x0 - xr) + math.sin(angulo) * (y0 - yr))
-   c = (x0 - xr) ** 2 + (y0 - yr) ** 2 - ROBOT_RADIUS**2
-   delta = (
-       b**2 - 4 * a * c
-   )
-   if delta >= 0:
-       return True
-   return False
+    a = 1
+    b = 2 * (math.cos(angulo) * (x0 - xr) + math.sin(angulo) * (y0 - yr))
+    c = (x0 - xr) ** 2 + (y0 - yr) ** 2 - ROBOT_RADIUS**2
+    delta = (
+        b**2 - 4 * a * c
+    )
+    if delta >= 0:
+        return True
+    return False
 
 
 """_summary_: Calcula os ângulos das retas tangentes que passam por (x0, y0) de
@@ -51,22 +48,22 @@ o ângulo oposto pelo vértice
 return _type_: tupla
 """
 def ang_tangent_lines(x0, y0, xr, yr, x_gol):
-   a = ROBOT_RADIUS**2 - (xr - x0) ** 2
-   b = -2 * (xr - x0) * (y0 - yr)
-   c = -((y0 - yr) ** 2) + ROBOT_RADIUS**2
-   delta = b**2 - 4 * a * c
-   if delta >= 0:
-       m1 = (-b + math.sqrt(delta)) / (2 * a)
-       m2 = (-b - math.sqrt(delta)) / (2 * a)
-       theta1 = math.atan(m1)
-       theta2 = math.atan(m2)
-       if x_gol < 0:
-           tmp = theta1
-           theta1 = -1 * theta2
-           theta2 = -1 * tmp
-       theta1 = min(theta1, theta2)
-       theta2 = max(theta1, theta2)
-       return theta1, theta2
+    a = ROBOT_RADIUS**2 - (xr - x0) ** 2
+    b = -2 * (xr - x0) * (y0 - yr)
+    c = -((y0 - yr) ** 2) + ROBOT_RADIUS**2
+    delta = b**2 - 4 * a * c
+    if delta >= 0:
+        m1 = (-b + math.sqrt(delta)) / (2 * a)
+        m2 = (-b - math.sqrt(delta)) / (2 * a)
+        theta1 = math.atan(m1)
+        theta2 = math.atan(m2)
+        if x_gol < 0:
+            tmp = theta1
+            theta1 = -1 * theta2
+            theta2 = -1 * tmp
+        theta1 = min(theta1, theta2)
+        theta2 = max(theta1, theta2)
+        return theta1, theta2
   
 """_summary_: Cria uma lista com os obstáculos que estão entre as retas que partem de (x0, y0)
 e terminam nos limites do gol. Para tanto, verifica se essas retas cortam algum bob ou se as retas
@@ -76,29 +73,36 @@ que partem de (x0, y0) e passam por (xr, yr) cruzam o gol.
 return _type_: list
 """
 def calc_visible_bobs(
-   x0, y0, obstacles_coord, x_gol, theta_max, theta_min
+   x0, y0, obstacles_coord, gol_center, theta_max, theta_min
 ):
-   visible_bobs = []
-   for i in range(len(obstacles_coord)):
-       xr = obstacles_coord[i].x
-       yr = obstacles_coord[i].y
-       if (x_gol > 0 and xr < 0) or (x_gol < 0 and xr > 0) or (xr == x0):
+    x_gol = gol_center.x
+    y_golMax =  gol_center.y + GOAL_LENGHT / 2
+    y_golMin =  gol_center.y - GOAL_LENGHT / 2
+    visible_bobs = []
+    for i in range(len(obstacles_coord)):
+        xr = obstacles_coord[i].x
+        yr = obstacles_coord[i].y
+        if xr == x0:
            continue
-       m = (yr - y0) / (xr - x0)
-       intersec_y = m * (x_gol - x0) + y0
-       if (intersec_y >= YGOL_MIN and intersec_y <= YGOL_MAX) or (
-           haIntersecao(xr, yr, x0, y0, theta_min)
-           or haIntersecao(xr, yr, x0, y0, theta_max)
-       ):
-           tangent_angles = ang_tangent_lines(x0, y0, xr, yr, x_gol)  # type: ignore
-           if tangent_angles is None:
-               continue
-           theta_bottom, theta_top = tangent_angles
-           bob = Obstacle(xr, yr, theta_bottom, theta_top)  # type: ignore
-           visible_bobs.append(bob)
+        if xr == x_gol:
+            theta_bottom, theta_top = tangent_angles
+            bob = Obstacle(xr, yr, theta_bottom, theta_top)  # type: ignore
+            visible_bobs.append(bob)
+            continue
+        m = (yr - y0) / (xr - x0)
+        intersec_y = m * (x_gol - x0) + y0
+        if (intersec_y >= y_golMin and intersec_y <= y_golMax) or (
+            haIntersecao(xr, yr, x0, y0, theta_min)
+            or haIntersecao(xr, yr, x0, y0, theta_max)
+        ):
+            tangent_angles = ang_tangent_lines(x0, y0, xr, yr, x_gol)  # type: ignore
+            if tangent_angles is None:
+                continue
+            theta_bottom, theta_top = tangent_angles
+            bob = Obstacle(xr, yr, theta_bottom, theta_top)  # type: ignore
+            visible_bobs.append(bob)
 
-
-   return visible_bobs
+    return visible_bobs
 
 
 """_summary_: Remove da lista dos robôs visíveis aqueles cujas tangentes estão entre as tangentes dos
@@ -108,18 +112,18 @@ demais obstáculos
 return _type_: void (altera a lista passada como parâmetro)
 """
 def removePontosCegos(visible_bobs):
-   to_remove = []
-   for bob in visible_bobs:
-       for i in range(len(visible_bobs)):
-           obs = visible_bobs[i]
-           if (
-               bob.theta_top < obs.theta_top and bob.theta_top > obs.theta_bottom
-           ) and (
-               bob.theta_bottom < obs.theta_top and bob.theta_bottom > obs.theta_bottom
-           ):
-               to_remove.append(bob)
-   for i in range(len(to_remove)):
-       visible_bobs.remove(to_remove[i])
+    to_remove = []
+    for bob in visible_bobs:
+        for i in range(len(visible_bobs)):
+            obs = visible_bobs[i]
+            if (
+                bob.theta_top < obs.theta_top and bob.theta_top > obs.theta_bottom
+            ) and (
+                bob.theta_bottom < obs.theta_top and bob.theta_bottom > obs.theta_bottom
+            ):
+                to_remove.append(bob)
+    for i in range(len(to_remove)):
+        visible_bobs.remove(to_remove[i])
 
 
 """_summary_: Identifica o maior ângulo da visão do gol. Primeiramente, calcula os obstáculos visíveis, remove
@@ -127,53 +131,56 @@ aqueles entre pontos cegos e organiza eles segundo o ângulo da tangente inferio
 superior de um obstáculo até a tangente superior do próximo.
 
 
-return _type_: float (módulo do ângulo em graus)
+return _type_: float (módulo do ângulo em radianos)
 """
-def limits_of_visibility(obstacles: list[Pose2D], p0: Pose2D, x_gol: float) -> list[float]:
-   x0 = p0.x
-   y0 = p0.y
+def limits_of_visibility(obstacles: list[Pose2D], p0: Pose2D, gol_center: Pose2D) -> list[float]:
+    x_gol = gol_center.x
+    y_golMax =  gol_center.y + GOAL_LENGHT / 2
+    y_golMin =  gol_center.y - GOAL_LENGHT / 2
+    x0 = p0.x
+    y0 = p0.y   
+
+    kick_angle = [0, 0]
+    if x_gol == x0:
+        return kick_angle
+
+    theta_max = math.atan((y_golMax - y0) / ((x_gol - x0)))
+    theta_min = math.atan((y_golMin - y0) / ((x_gol - x0)))
+    if x_gol < 0:
+        theta_min *= -1
+        theta_max *= -1
+    theta = theta_min
 
 
-   kick_angle = [0, 0]
-   if x_gol == x0:
-       return kick_angle
+    visible_bobs = calc_visible_bobs(
+        x0, y0, obstacles, gol_center, theta_max, theta_min
+    )
+    removePontosCegos(visible_bobs)
+    visible_bobs.sort(
+        key=lambda bob: bob.theta_bottom
+    )
+
+    if not visible_bobs:
+        kick_angle = [theta, theta_max]
+    else:
+        for i in range(len(visible_bobs)):
+            theta_bottom = visible_bobs[i].theta_bottom
+            theta_top = visible_bobs[i].theta_top
+            intervalo = theta_bottom - theta
+            if intervalo >= 0 and intervalo > (kick_angle[1] - kick_angle[0]):
+                kick_angle = [theta, theta_bottom]
+            theta = theta_top
+            if theta >= theta_max:
+                break
+            elif (i == len(visible_bobs) - 1) and theta < theta_max:
+                if theta_max - theta > kick_angle[1] - kick_angle[0]:
+                    kick_angle = [theta, theta_max]
+    if(kick_angle[1] - kick_angle[0]) >= math.radians(VISIBILIDADE_MIN):
+        return kick_angle
+    else:
+        return [0,0]
 
 
-   theta_max = math.atan((YGOL_MAX - y0) / ((x_gol - x0)))
-   theta_min = math.atan((YGOL_MIN - y0) / ((x_gol - x0)))
-   if x_gol < 0:
-       theta_min *= -1
-       theta_max *= -1
-   theta = theta_min
-
-
-   visible_bobs = calc_visible_bobs(
-       x0, y0, obstacles, x_gol, theta_max, theta_min
-   )
-   removePontosCegos(visible_bobs)
-   visible_bobs.sort(
-       key=lambda bob: bob.theta_bottom
-   )
-
-
-   if not visible_bobs:
-       kick_angle = [theta, theta_max]
-   else:
-       for i in range(len(visible_bobs)):
-           theta_bottom = visible_bobs[i].theta_bottom
-           theta_top = visible_bobs[i].theta_top
-           intervalo = theta_bottom - theta
-           if intervalo >= 0 and intervalo > (kick_angle[1] - kick_angle[0]):
-               kick_angle = [theta, theta_bottom]
-           theta = theta_top
-           if theta >= theta_max:
-               break
-           elif (i == len(visible_bobs) - 1) and theta < theta_max:
-               if theta_max - theta > kick_angle[1] - kick_angle[0]:
-                   kick_angle = [theta, theta_max]
-   return kick_angle
-
-
-def max_range_of_visibility(obstacles: list[Pose2D], p0: Pose2D, x_gol: float) -> float:
-   kick_angle = limits_of_visibility(obstacles, p0, x_gol)
-   return abs(kick_angle[1] - kick_angle[0])
+def max_range_of_visibility(obstacles: list[Pose2D], p0: Pose2D, gol_center: Pose2D) -> float:
+    kick_angle = limits_of_visibility(obstacles, p0, gol_center)
+    return abs(kick_angle[1] - kick_angle[0])
